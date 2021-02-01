ceda3eee6bc240c0929f2f932b29df08c7d47be7
hally255 <37000292+hally255@users.noreply.github.com>
2021-02-01 15:14:08 -0500

    lab_03

---
+++ lab_03/lab03/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './hompage';
+
+function App() {
+  const fname = "Magnus";
+  return (
+    <HomePage firstName={fname} />
+  )
+}
+
+export default App;
